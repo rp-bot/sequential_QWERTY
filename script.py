36b88256e1b8bb373595af008ab184f08228ca98
rp-bot_ubuntu <legacy.business18@gmail.com>
2021-12-30 11:01:47 -0500

    _

---
+++ script.py
@@ -0,0 +1,6 @@
+import re
+random_gib_shit = input("enter random shit: ")
+
+gib_split = list(random_gib_shit)
+gib_sorted = sorted(set(random_gib_shit))
+print([gib_split.count(i) for i in gib_sorted])
