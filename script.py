f7b57afe3e2d44e6d845d4b5da87b40d3c82e544
rp-bot_ubuntu <legacy.business18@gmail.com>
2021-12-30 22:45:15 -0500

    unique char set

--- script.py
+++ script.py
@@ -3,4 +3,11 @@ random_gib_shit = input("enter random shit: ")
 
 gib_split = list(random_gib_shit)
 gib_sorted = sorted(set(random_gib_shit))
-print([gib_split.count(i) for i in gib_sorted])
+unique_set = [gib_split.count(i) for i in gib_sorted]
+
+combined_dict = dict(zip(gib_sorted, unique_set))
+# list(combined_dict.values()) # prints .values or .keys
+
+print(gib_sorted)
+print(unique_set)
+print(combined_dict)
