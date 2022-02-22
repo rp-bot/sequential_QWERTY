6efaf81fd056737fadc800156dd18328d8532f31
rp-bot_ubuntu <legacy.business18@gmail.com>
2022-02-22 17:02:08 -0500

    gitignore update

--- piano/src/components/Piano.js
+++ piano/src/components/Piano.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Key } from './Key.js'
 
 class Piano extends React.Component{
     render(){
         return(
          <div>
-             <h1>
-                 PIANO!
-             </h1>
+            <h1>PIANO!</h1>
+            <Key/>
+            <Key/>
+            <Key/>
          </div>   
         )
     }
